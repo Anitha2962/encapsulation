e0a76376927ed52fdd253a985383b25cb841cee7
anitha2962 <anithasivadas92@gmail.com>
2024-02-02 19:43:00 +0530

    'first-commit'

---
+++ DELEGATE/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//class program
+//{
+//    delegate int changeNumber(int x);
+//    static void main(string[] args)
+//    {
+//        changeNumber del1 = new changeNumber(add10);
+//        Console.WriteLine(del1.Invoke(20));
+//        changeNumber del2 = new changeNumber(multiply10);
+//        Console.WriteLine(del2.Invoke(100));
+//        Console.WriteLine(del2(100));
+//    }
+//    static int add10(int n);
+//        {
+//        return n+10;
+//        }
+//static int multiply10 (int n);
+//    {
+//    return n*10;
+//}
+
+
+
